6674fadda3e654af314dea19425ebcca8ff5fac8
Hariswar8018 <hariswarsamasi@gmail.com>
2026-05-12 10:02:26 +0530

    chnages fix

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from flask import redirect
 from flask import Flask, render_template
+from flask import send_from_directory
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -24,7 +27,7 @@ def ios_app():
 
 @app.route('/android-app')
 def android_app():
-    return render_template('android_app.html')
+    return redirect("https://play.google.com/store/apps/details?id=com.ignito.filedockuser")
 
 @app.route('/privacy')
 def privacy():
@@ -33,6 +36,19 @@ def privacy():
 @app.route('/terms')
 def terms():
     return render_template('terms.html')
+
+@app.route('/.well-known/assetlinks.json')
+def assetlinks():
+    return send_from_directory(
+        'static/.well-known',
+        'assetlinks.json',
+        mimetype='application/json'
+    )
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
